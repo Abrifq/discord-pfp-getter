c65339723dd2ffb880102d34d2245d6fa0f3a273
Arda Aydın <arda.aydin@operationsilkscarf.com>
2021-06-15 03:11:10 +0300

    updated submodule

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const server = require("./gitmodules/serve-module"),
     { apiHandler: getPfpApi } = require("./api/get-profile-picture");
 
 server.apiPageHandlers.add("/api/getpfp", getPfpApi);
-server.staticServer.servePath = "./web/";
-server.staticServer.enableServer = true;
+server.staticServer.searchPath = "./web/";
 server.port = require("process").env["PORT"] || 8080;
 server.startServer();
